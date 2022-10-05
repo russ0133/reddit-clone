7ffc3e7a32c269a50fac3c3063060fe68fc909b5
Gabriel <gabrieldsfs@gmail.com>
2022-10-05 18:49:31 +0500

    mikro orm and postgresql initial

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,16 @@
+import { MikroORM } from "@mikro-orm/core";
+import { __prod__ } from "./constants";
+
+const main = async () => {
+  const orm = await MikroORM.init({
+    entities: [],
+    dbName: "reddit",
+    user: "postgres",
+    password: "password",
+    type: "postgresql",
+    debug: !__prod__,
+  });
+};
+
 console.log("hello there");
+main();
