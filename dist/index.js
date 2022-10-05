5746e731fd35dc35634cc49fd215e21fd2c96546
Gabriel <gabrieldsfs@gmail.com>
2022-10-05 20:43:31 +0500

    set up Post resolvers

--- dist/index.js
+++ dist/index.js
@@ -9,20 +9,21 @@ const express_1 = __importDefault(require("express"));
 const apollo_server_express_1 = require("apollo-server-express");
 const type_graphql_1 = require("type-graphql");
 const hello_1 = require("./resolvers/hello");
+const post_1 = require("./resolvers/post");
 const main = async () => {
     const orm = await core_1.MikroORM.init(mikro_orm_config_1.default);
     await orm.getMigrator().up();
+    const em = orm.em.fork();
     const app = (0, express_1.default)();
-    const apolloServer = new apollo_server_express_1.ApolloServer({ schema: await (0, type_graphql_1.buildSchema)({ resolvers: [hello_1.HelloResolver], validate: false }) });
+    const apolloServer = new apollo_server_express_1.ApolloServer({
+        schema: await (0, type_graphql_1.buildSchema)({ resolvers: [hello_1.HelloResolver, post_1.PostResolver], validate: false }),
+        context: () => ({ em: em }),
+    });
     await apolloServer.start();
     apolloServer.applyMiddleware({ app });
-    app.get("/", (_, res) => {
-        res.send("hello");
-    });
     app.listen(4000, () => {
         console.log("server started on localhost:4000");
     });
 };
-console.log("hello there");
 main().catch((err) => console.error(err));
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -5,27 +5,26 @@ import express from "express";
 import { ApolloServer } from "apollo-server-express";
 import { buildSchema } from "type-graphql";
 import { HelloResolver } from "./resolvers/hello";
+import { PostResolver } from "./resolvers/post";
 
 const main = async () => {
   const orm = await MikroORM.init(microConfig);
   await orm.getMigrator().up();
-  //const emFork = orm.em.fork();
+  const em = orm.em.fork();
 
   const app = express();
 
-  const apolloServer = new ApolloServer({ schema: await buildSchema({ resolvers: [HelloResolver], validate: false }) });
+  const apolloServer = new ApolloServer({
+    schema: await buildSchema({ resolvers: [HelloResolver, PostResolver], validate: false }),
+    context: () => ({ em: em }),
+  });
 
   await apolloServer.start();
   apolloServer.applyMiddleware({ app });
-
-  app.get("/", (_, res) => {
-    res.send("hello");
-  });
 
   app.listen(4000, () => {
     console.log("server started on localhost:4000");
   });
 };
 
-console.log("hello there");
 main().catch((err) => console.error(err));
